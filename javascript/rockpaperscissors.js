118d4c34f116e406da7e3dbd53d7e8e4ba7639a4
Outoftheblue254 <chastainj80@gmail.com>
2025-04-22 15:10:23 -0500

    Created index file and js file

---
+++ javascript/rockpaperscissors.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+// This is a simple Rock, Paper, Scissors game
+// where the user plays against the computer.
+// The user can choose Rock, Paper, or Scissors, and the computer will randomly choose one of the three options.
+function getComputerChoice() {
+    
+}
